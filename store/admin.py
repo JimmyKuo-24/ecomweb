b333d7f5a66298dd2083921ef956b3a1ba2e495a
JIMMY GUO <57852430+JimmyKuo-24@users.noreply.github.com>
2024-06-10 00:35:00 +0900

    Add files via upload

---
+++ store/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Product, Cart, CartItem, Order, OrderItem
+
+# Register your models here.
+
+admin.site.register(Product)
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
